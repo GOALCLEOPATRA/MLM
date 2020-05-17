7b3ed6ed0a673768186ccbe7566f461cb77d6025
JasonArmitage-res <jason.armitage@ontotext.com>
2020-05-17 16:41:42 +0200

    model_t2 updated to match model_mtl

--- models/model_t2.py
+++ models/model_t2.py
@@ -18,12 +18,13 @@ class CoordNet(nn.Module):
     def __init__(self, tags=args.coordDim, dropout=args.dropout):
         super(CoordNet, self).__init__()
         self.coord_net = nn.Sequential(
-            nn.LSTM(input_size=args.embDim, hidden_size=args.embDim, bidirectional=False, batch_first=True),
-            LstmFlatten(),
+            nn.Linear(args.embDim, args.embDim),
+            nn.ReLU(),
+            nn.Flatten(),
             nn.Dropout(dropout),
             nn.Linear(args.embDim, tags)
-            #nn.LogSoftmax(dim=1)
         )
+
     def forward(self, x):
         return self.coord_net(x.unsqueeze(1))
 
@@ -75,36 +76,6 @@ class LearnTriples(nn.Module):
 
     def forward(self, x):
         return self.embedding(x.unsqueeze(1))
-
-# mtl model
-class L2Joint(nn.Module):
-    def __init__(self):
-        super(L2Joint, self).__init__()
-        self.mml_emp = torch.Tensor([True, False])
-        self.n_mml = len(self.mml_emp)
-        self.log_vars = torch.nn.Parameter(torch.zeros(self.n_mml))
-        #self.mse = nn.MSELoss()
-        self.t1_wa = torch.Tensor(1).requires_grad_(True)
-        self.t2_wa = torch.Tensor(1).requires_grad_(True)
-
-    def forward(self, m_loss_t1, m_loss_t2):
-        t1 = m_loss_t1
-        t2 = m_loss_t2
-
-        # weighted average
-        mm_losses = torch.stack((t1, t2))
-        dtype = mm_losses.dtype
-        device = mm_losses.device
-        stds = (torch.exp(self.log_vars)**(1/2)).to(device).to(dtype)
-        weights = 1 / ((self.mml_emp.to(device).to(dtype)+1)*(stds**2))
-        wa_losses = weights*mm_losses + torch.log(stds)
-        wa_losses_mn = wa_losses.mean()
-        t1_wa = wa_losses[0]
-        t2_wa = wa_losses[1]
-        out = (wa_losses_mn, t1_wa, t2_wa)
-   
-        return out
-
 
 # MLM model
 class MLMRetrieval(nn.Module):
